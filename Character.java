8cf562fcc14abe4f54cf93053b34d4aa61cf8068
BobbyFisherman <basilrules2@gmail.com>
2026-03-16 08:21:18 -0500

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,14 @@
+
+
+public class Character{
+  int players;
+  String Name;
+  int age;
+
+  public Character(int age,int players,String Name){
+           this.players=players;
+           this.Name=Name;
+           this.age=age;
+  }
+
+}
